89aa2c6e233244f9030eec030dce26ef915fb15d
phankons <stephen.han@gmx.com>
2023-09-26 14:39:05 -0400

    EDD Rails Example - Part 1

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
-
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
+  password: ENV["HTTP_BASIC_PASSWORD"]
+  
   def index
     @products = Product.all.order(created_at: :desc)
   end
---
+++ db/migrate/20210626062916_create_sales.rb
@@ -0,0 +1,13 @@
+class CreateSales < ActiveRecord::Migration[6.1]
+  def change
+    create_table :sales do |t|
+
+      t.string :name
+      t.integer :percent_off
+      t.date :starts_on
+      t.date :ends_on
+
+      t.timestamps null: false
+    end
+  end
+end
